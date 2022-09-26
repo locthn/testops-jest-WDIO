cab6a3887eebac338e0d346cd8f82f099be61273
locthn <loc.thnguyen@katalon.com>
2022-09-26 15:01:25 +0700

    more file

--- sample.test.js
+++ sample.test.js
@@ -36,39 +36,3 @@ describe('Run test on TestOps', () =>{
     })
 })
 
-describe('Run test on TestOps 2', () =>{
-    test('run test newz', () =>{
-        expect(0).toEqual(0)
-    })
-
-    test('run test failedz', () => {
-        expect(1).toEqual(1)
-    })
-
-    test('run test 2z', () =>{
-        expect(0).toEqual(0)
-    })
-    test('run test 3z', () =>{
-        expect(0).toEqual(0)
-    })
-    test('run test 4z', () =>{
-        expect(0).toEqual(0)
-    })
-    test('run test 6z', () =>{
-        expect(0).toEqual(0)
-    })
-
-    test('run test 7z', () => {
-        expect(1).toEqual(1)
-    })
-
-    test('run test 8z', () =>{
-        expect(0).toEqual(0)
-    })
-    test('run test 9z', () =>{
-        expect(0).toEqual(0)
-    })
-    test('run test 10z', () =>{
-        expect(0).toEqual(0)
-    })
-})
