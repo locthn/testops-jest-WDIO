008ce49912eb5eb67e647ce20b4dcd66e04340d2
locthn <loc.thnguyen@katalon.com>
2022-09-26 11:43:46 +0700

    init project

---
+++ sample.test.js
@@ -0,0 +1,10 @@
+
+describe('Run test on TestOps', () =>{
+    test('run test 1000k', () =>{
+        expect(0).toEqual(0)
+    })
+
+    test('run test failed', () => {
+        expect(1).toEqual(2)
+    } )
+})
