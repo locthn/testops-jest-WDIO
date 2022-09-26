e50d9bb07b1c0986caa9e8166e463724b7d68781
locthn <loc.thnguyen@katalon.com>
2022-09-26 14:58:08 +0700

    add more case

--- sample.test.js
+++ sample.test.js
@@ -1,10 +1,37 @@
 
 describe('Run test on TestOps', () =>{
-    test('run test 1000k', () =>{
+    test('run test new', () =>{
         expect(0).toEqual(0)
     })
 
     test('run test failed', () => {
-        expect(1).toEqual(2)
-    } )
+        expect(1).toEqual(1)
+    })
+
+    test('run test 2', () =>{
+        expect(0).toEqual(0)
+    })
+    test('run test 3', () =>{
+        expect(0).toEqual(0)
+    })
+    test('run test 4', () =>{
+        expect(0).toEqual(0)
+    })
+    test('run test 6', () =>{
+        expect(0).toEqual(0)
+    })
+
+    test('run test 7', () => {
+        expect(1).toEqual(1)
+    })
+
+    test('run test 8', () =>{
+        expect(0).toEqual(0)
+    })
+    test('run test 9', () =>{
+        expect(0).toEqual(0)
+    })
+    test('run test 10', () =>{
+        expect(0).toEqual(0)
+    })
 })
